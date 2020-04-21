5039404e41d2f7702a3f1fd60ffefe1956d873d9
kreloaded <kiran@ost.com>
2020-04-21 12:33:02 +0530

    Added search for a key function in BST

--- Trees/BST/BST.java
+++ Trees/BST/BST.java
@@ -16,8 +16,8 @@ public class BST {
 		Scanner sc = new Scanner(System.in);
 
 		int choice = 0;
-		while(choice < 5) {
-			System.out.println("Choose operation to perform: \n1. Insert\n2. Inorder Traversal\n3. Pre-order Traversal\n4. Post-order Traversal");
+		while(choice < 6) {
+			System.out.println("Choose operation to perform: \n1. Insert\n2. Inorder Traversal\n3. Pre-order Traversal\n4. Post-order Traversal\n5. Search for a key");
 			choice = sc.nextInt();
 
 			switch(choice) {
@@ -37,6 +37,16 @@ public class BST {
 
 				case 4: System.out.println("Post-order traversal of the tree is :");
 						postOrder(root);
+						break;
+
+				case 5: System.out.println("Enter key to be searched :");
+						int key = sc.nextInt();
+						Node result = searchKey(root, key);
+						if(result != null) {
+							System.out.println("Key found !");
+						} else {	
+							System.out.println("Key not found !");
+						}
 						break;
 			}
 		}
@@ -81,5 +91,15 @@ public class BST {
 			System.out.println(current.val);
 		}
 	}
+
+	private static Node searchKey(Node root, int key) {
+		if (root == null || root.val == key) {
+			return root; 
+		}
+    	if (root.val > key) {
+        	return searchKey(root.left, key); 
+    	}
+    	return searchKey(root.right, key); 
+	} 
 
 }
